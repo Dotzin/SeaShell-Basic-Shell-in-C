e3ecef869d8faba7172810003ab90e96f623ad75
DOTzin <augustoeliasmmribas@gmail.com>
2026-03-26 22:03:51 -0300

    feat: SeaShell loop  output

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
-void main()
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
 {
+    char *line;
+    char **args;
+    int status = 1;
+    do
+    {
+        printf("Sea >");
+    } while (status==1);
     
+    
+    return EXIT_SUCCESS;
 }
