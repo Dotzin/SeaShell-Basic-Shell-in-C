8fcac01f31ec2f981981bc94cb150ffb8b225340
DOTzin <augustoeliasmmribas@gmail.com>
2026-03-26 22:39:35 -0300

    feat: Added split_line function logic

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #define TOK_DELIM " \t\r\n"
 #define RED "\033[0;31m"
 #define RESET "\e[0m"
+#define TK_BUFF_SIZE 1024
 
 
 char *read_line();
@@ -53,7 +54,7 @@ char * read_line()
         position++;
         if(position >= buffsize)
         {
-            buffsize += 1024;
+            buffsize += TK_BUFF_SIZE;
             buffer = realloc(buffer , buffsize);
 
             if(!buffer)
@@ -66,3 +67,40 @@ char * read_line()
     
 }
 
+char ** split_line(char * line)
+{
+    int buffsize = TK_BUFF_SIZE;
+    int position = 0;
+
+    char ** tokens = malloc(buffsize * sizeof(char*));
+    char * token;
+
+    if(!tokens)
+    {
+        fprintf(stderr, "%sdash: Allocation error%s\n", RED, RESET);
+        exit(EXIT_FAILURE);
+    }
+    token = strtok(line, TOK_DELIM);
+    
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+        if(position >= buffsize)
+        {
+            buffsize += TK_BUFF_SIZE;
+            tokens = realloc(tokens, buffsize* sizeof(char *));
+
+            if(!tokens)
+            {
+                fprintf(stderr, "%sdash: Allocation error%s\n", RED, RESET);
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, TOK_DELIM);
+    }
+    tokens[position] = NULL;
+
+    return tokens;
+}
